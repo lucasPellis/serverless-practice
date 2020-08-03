811da0b77bfca7cdf496d4ebdd92d865a16484d2
Lucas Pellis <lucaspellis@hotmail.com>
2020-08-02 21:26:42 -0300

    change API url

--- api/bands.js
+++ api/bands.js
@@ -1,17 +1,17 @@
 module.exports = (req, res) => {
-    const bands = [
-      {
-        name: 'Band 1',
-        genre: 'Genre 1'
-      },
-      {
-        name: 'Band 2',
-        genre: 'Genre 2'
-      },
-      {
-        name: 'Band 3',
-        genre: 'Genre 3'
-      },
-    ]
-    res.end(JSON.stringify(bands))
-  }
+  const bands = [
+    {
+      name: 'Dio',
+      genre: 'Heavy Metal'
+    },
+    {
+      name: 'Anthrax',
+      genre: 'Trash Metal'
+    },
+    {
+      name: 'Tenebrarum',
+      genre: 'Gothic Metal'
+    }
+  ]
+  res.end(JSON.stringify(bands))
+}
